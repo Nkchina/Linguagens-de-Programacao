fb1a8c50a7b5cbdb1b93222bebfa1bd72a1fc3a7
Nicolas Chi <94817330+Nkchina@users.noreply.github.com>
2023-03-07 01:03:29 -0300

    Add files via upload

---
+++ Telefone.java
@@ -0,0 +1,29 @@
+package br.com.newton.main;
+import br.com.newton.agenda.Contato;
+import java.util.Scanner;
+
+public class Telefone {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        Contato Contato = new Contato();
+
+        System.out.print("Digite o nome do contato: ");
+        String nome = ler.nextLine();
+
+        System.out.print("Digite o ddd do contato: ");
+        int ddd = ler.nextInt();
+
+        System.out.print("Digite o número do contato: ");
+        int telefone = ler.nextInt();
+
+        if(telefone<9){
+            System.out.print("Telefone inválido!");
+        }
+        else{
+            System.out.println("Contato adicionado!");
+            System.out.println("Nome: " + nome);
+            System.out.println("Telefone: " + ddd + telefone);
+        }
+
+    }
+}
