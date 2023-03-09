2a04d882c5ee5e61c1adff20b482878246293ad4
Nicolas Chi <94817330+Nkchina@users.noreply.github.com>
2023-03-09 19:33:41 -0300

    Update Contato.java

--- Agenda/Contato.java
+++ Agenda/Contato.java
@@ -1,4 +1,10 @@
 package br.com.newton.agenda;
 
 public class Contato {
+  private String nome;
+  private String telefone;
+  
+  public Contato(String nome, String telefone){
+    this.nome = nome;
+    this.telefone = telefone;
 }
