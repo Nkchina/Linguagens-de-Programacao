5d241b536693079e90f74a9d9b3c25d666bb32be
Nicolas Chi <94817330+Nkchina@users.noreply.github.com>
2023-03-09 20:25:56 -0300

    Update Telefone.java

--- Agenda/Telefone.java
+++ Agenda/Telefone.java
@@ -9,21 +9,33 @@ public class Telefone {
 
         System.out.print("Digite o nome do contato: ");
         String nome = ler.nextLine();
+        contato1.setNome(nome);
 
         System.out.print("Digite o ddd do contato: ");
-        int ddd = ler.nextInt();
+        String ddd = ler.nextLine();
+        contato1.setDdd(ddd);
 
         System.out.print("Digite o número do contato: ");
-        int telefone = ler.nextInt();
+        String telefone = ler.nextLine();
+        contato1.setTelefone(telefone);
 
-        if(telefone<9){
-            System.out.print("Telefone inválido!");
+        if (contato1.getTelefone().length() != 9) {
+            System.out.println("Telefone inválido!");
         }
-        else{
+
+        else {
             System.out.println("Contato adicionado!");
             System.out.println("Nome: " + nome);
-            System.out.println("Telefone: " +"(" + ddd + ")" + telefone);
+            System.out.println("Telefone: " + "(" + ddd + ")" + telefone);
         }
+
+        System.out.println("Digite o contato para excluir: ");
+        String nomeE = ler.nextLine();
+        contato1.excluir(nomeE);
+
+        System.out.println("Digite o contato para adicionar: ");
+        String add = ler.nextLine();
+        contato1.add(add);
 
     }
 }
