a5cf3dca7820268d3a1fbdcb017f2b6e2e300332
Nicolas Chi <94817330+Nkchina@users.noreply.github.com>
2023-04-16 22:33:34 -0300

    Update Massa.java

--- Cantina/Massa.java
+++ Cantina/Massa.java
@@ -1,18 +1,20 @@
 public class Massa {
-    private final String tipo;
-    private final double valor;
-
-    public Massa(String tipo, double valor) {
-        this.tipo = tipo;
-        this.valor = valor;
-    }
+    private String tipo;
+    private double valor;
 
     public String getTipo() {
         return tipo;
     }
 
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
     public double getValor() {
         return valor;
     }
-}
 
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
