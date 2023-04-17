ce793abb5a34ad2eb49a9d06f5226ee09ce95ce7
Nicolas Chi <94817330+Nkchina@users.noreply.github.com>
2023-04-16 22:32:54 -0300

    Update Ingrediente.java

--- Cantina/Ingrediente.java
+++ Cantina/Ingrediente.java
@@ -1,11 +1,11 @@
 public class Ingrediente {
-    private final String nome;
-
-    public Ingrediente(String nome) {
-        this.nome = nome;
-    }
+    private String nome;
 
     public String getNome() {
         return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 }
