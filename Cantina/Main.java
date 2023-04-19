edaf7a3b4102b9e41d167be631abf96cc41cae2f
Nicolas Chi <94817330+Nkchina@users.noreply.github.com>
2023-04-19 16:12:44 -0300

    Update Main.java

--- Cantina/Main.java
+++ Cantina/Main.java
@@ -4,12 +4,14 @@ import javax.swing.JOptionPane;
 public class Main {
     public static void main(String[] args) {
         String nomeCliente = JOptionPane.showInputDialog("Informe o nome do cliente:");
+        Cliente cliente = new Cliente(nomeCliente);
 
         String nomeMassa = JOptionPane.showInputDialog("Informe o nome da massa:");
         double valorMassa = Double.parseDouble(JOptionPane.showInputDialog("Informe o valor da massa:"));
+        Massa massa = new Massa(nomeMassa, valorMassa);
 
         String[] opcoesIngredientes = {"Bacon", "Mussarela", "Tomate", "Queijo"};
-        ArrayList<String> ingredientesEscolhidos = new ArrayList<String>();
+        ArrayList<Ingrediente> ingredientesEscolhidos = new ArrayList<Ingrediente>();
         double valorIngredientes = 0;
 
         while (true) {
@@ -20,18 +22,23 @@ public class Main {
                 break;
             }
 
-            ingredientesEscolhidos.add(opcoesIngredientes[escolha]);
+            Ingrediente ingrediente = new Ingrediente(opcoesIngredientes[escolha]);
+            ingredientesEscolhidos.add(ingrediente);
             valorIngredientes += 2.0;
         }
 
-        double valorTotal = valorMassa + valorIngredientes;
+        ArrayList<Ingrediente> ingredientes = ingredientesEscolhidos;
+        Pedido pedido = new Pedido(massa, cliente, ingredientes);
 
-        String mensagem = "Pedido feito para " + nomeCliente + "\n" + nomeMassa + " com ";
-        for (String ingrediente : ingredientesEscolhidos) {
-            mensagem += ingrediente + " e ";
+        double valorTotal = pedido.calcularValor();
+
+        String mensagem = "Pedido feito para " + pedido.getCliente().getNome() + "\n" + pedido.getMassa().getTipo() + " com ";
+        for (Ingrediente ingrediente : pedido.getIngredientes()) {
+            mensagem += ingrediente.getNome() + " e ";
         }
         mensagem = mensagem.substring(0, mensagem.length() - 3); // Remover o último " e "
         mensagem += "\nValor: " + String.format("%.2f", valorTotal);
+
         JOptionPane.showMessageDialog(null, mensagem);
     }
 }
