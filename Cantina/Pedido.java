9a4ce63010009d11594887fb3aff2b51f3c842cd
Nicolas Chi <94817330+Nkchina@users.noreply.github.com>
2023-04-19 16:02:40 -0300

    Update Pedido.java

--- Cantina/Pedido.java
+++ Cantina/Pedido.java
@@ -1,14 +1,43 @@
 import java.util.ArrayList;
-import java.util.List;
+
 public class Pedido {
-    Massa massa;
-    Cliente cliente;
+    private Massa massa;
+    private Cliente cliente;
+    private ArrayList<Ingrediente> ingredientes;
 
-    List <Ingrediente> Lista = new ArrayList<Ingrediente>();
-
-    public double calculPedido (){
-
-        return massa.getValor();
+    public Pedido(Massa massa, Cliente cliente, ArrayList<Ingrediente> ingredientes) {
+        this.massa = massa;
+        this.cliente = cliente;
+        this.ingredientes = ingredientes;
     }
 
+    public double calcularValor() {
+        double valor = massa.getValor();
+        valor += ingredientes.size() * 2.0;
+        return valor;
+    }
+
+    public Massa getMassa() {
+        return massa;
+    }
+
+    public void setMassa(Massa massa) {
+        this.massa = massa;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public ArrayList<Ingrediente> getIngredientes() {
+        return ingredientes;
+    }
+
+    public void setIngredientes(ArrayList<Ingrediente> ingredientes) {
+        this.ingredientes = ingredientes;
+    }
 }
