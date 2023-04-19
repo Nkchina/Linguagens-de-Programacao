a4884dfd52082fc289d7edcdb033d3573dfdcd3c
Nicolas Chi <94817330+Nkchina@users.noreply.github.com>
2023-04-19 16:06:46 -0300

    Update Main.java

--- Cantina/Main.java
+++ Cantina/Main.java
@@ -1,20 +1,37 @@
+import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
-public class Main {
+public class Cantina {
     public static void main(String[] args) {
-        Pedido p = new Pedido();
-        String nome,massa;
-        double valor;
+        String nomeCliente = JOptionPane.showInputDialog("Informe o nome do cliente:");
 
-        nome = JOptionPane.showInputDialog("Informe o nome do Cliente: ");
-        p.cliente.setNome(nome);
-        massa = JOptionPane.showInputDialog("Informe o nome da massa: ");
-        p.massa.setTipo(massa);
-        valor = Double.parseDouble(JOptionPane.showInputDialog("Informe o valor da Massa: "));
-        p.massa.setValor(valor);
+        String nomeMassa = JOptionPane.showInputDialog("Informe o nome da massa:");
+        double valorMassa = Double.parseDouble(JOptionPane.showInputDialog("Informe o valor da massa:"));
 
-        String opcao;
-        int finalizar=0;
+        String[] opcoesIngredientes = {"Bacon", "Mussarela", "Tomate", "Queijo"};
+        ArrayList<String> ingredientesEscolhidos = new ArrayList<String>();
+        double valorIngredientes = 0;
 
+        while (true) {
+            int escolha = JOptionPane.showOptionDialog(null, "Escolha os ingredientes:", "Cantina",
+                    JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, opcoesIngredientes, opcoesIngredientes[0]);
+
+            if (escolha == opcoesIngredientes.length) {
+                break;
+            }
+
+            ingredientesEscolhidos.add(opcoesIngredientes[escolha]);
+            valorIngredientes += 2.0;
+        }
+
+        double valorTotal = valorMassa + valorIngredientes;
+
+        String mensagem = "Pedido feito para " + nomeCliente + "\n" + nomeMassa + " com ";
+        for (String ingrediente : ingredientesEscolhidos) {
+            mensagem += ingrediente + " e ";
+        }
+        mensagem = mensagem.substring(0, mensagem.length() - 3); // Remover o último " e "
+        mensagem += "\nValor: " + String.format("%.2f", valorTotal);
+        JOptionPane.showMessageDialog(null, mensagem);
     }
 }
